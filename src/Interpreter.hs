a7081bcb182a818e75b241726f2cb653e3ba18c4
Nikolas <nikmaehl@uni-bremen.de>
2020-01-26 23:26:58 +0100

    wip

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DerivingVia #-}
 module Interpreter where
 
 import Control.Monad
@@ -6,11 +7,13 @@ import Control.Monad.Trans.State.Lazy (StateT)
 import qualified Control.Monad.Trans.State.Lazy as StateT
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.Array (Array)
+import qualified Data.Array as Array
 
 import AST
 
-data IxIdt = IxIdt Int
-    deriving (Eq, Ord)
+newtype IxIdt = IxIdt Int
+    deriving (Eq, Ord, Show, Array.Ix) via Int
 
 data IxFun 
     = IxOpFun Operator IxIdt IxIdt
@@ -62,29 +65,35 @@ type Interpreter = InterpreterVal Int
 
 data InterpreterState = InterpreterState 
     { upperBound :: Int
-    , functionDictionary :: Map Idt Fun
-    , cache :: Map (Idt, Int) Int
+    , functionDictionary :: Map IxIdt IxFun
+    , cache :: Array (IxIdt, Int) (Maybe Int)
     }
 
-interpret' :: Map Idt Equation -> Map Idt [Int] -> Int -> Idt -> Int -> Either String Int
-interpret' definitions predefineds magnificationBound f x = do
+cacheLookup :: IxIdt -> Int -> InterpreterVal (Maybe Int)
+cacheLookup fIdt x = do
+    c <- cache <$> StateT.get 
+    return $ c Array.! (fIdt, x)
+
+cacheWrite :: IxIdt -> Int -> Int -> InterpreterVal ()
+cacheWrite fIdt x y = do
+    let updCache = \c -> c Array.// [((fIdt, x), Just y)]
+    let updState = \s -> s {cache = updCache (cache s)}
+    StateT.modify updState
+
+interpret :: Map Idt Equation -> Map Idt [Int] -> Int -> Idt -> Int -> Either String Int
+interpret definitions predefineds magnificationBound f x = do
     n <- lookupN predefineds
     let uB = n * magnificationBound
-    (idtIndex, functionIndex) <- indexDictionary $ buildDictionary definitions predefineds 
-    return 1
+    (idtIndex, functionIndex) <- indexDictionary $ buildDictionary definitions predefineds
+    let maxIdtIdx = length $ Map.keys idtIndex
+    let emptyCache = Array.listArray ((IxIdt 0, 0), (IxIdt maxIdtIdx, uB)) (replicate (maxIdtIdx * uB) Nothing)
+    let initialState = InterpreterState uB functionIndex emptyCache
+    let ixF = idtIndex Map.! f
+    StateT.evalStateT (evaluate ixF x) initialState
 
 lookupN :: Map Idt [Int] -> Either String Int 
 lookupN predefineds = case Map.lookup constN predefineds of
     (Just (n : _)) -> return n
-    otherwise -> Left $ "Error: undefined function symbol " ++ show constN
-
-interpret :: Map Idt Equation -> Map Idt [Int] -> Int -> Idt -> Int -> Either String Int
-interpret definitions predefineds magnificationBound f x = case Map.lookup constN predefineds of
-    (Just (n : _)) -> let 
-            dict = buildDictionary definitions predefineds
-            uB = n * magnificationBound
-            initialState = InterpreterState uB dict Map.empty
-        in StateT.evalStateT (evaluate f x) initialState
     otherwise -> Left $ "Error: undefined function symbol " ++ show constN
 
 buildDictionary :: Map Idt Equation -> Map Idt [Int] -> Map Idt Fun
@@ -107,35 +116,39 @@ inBounds x = do
     uB <- upperBound <$> StateT.get
     return $ 0 <= x && x < uB - 1
 
-evaluate :: Idt -> Int -> Interpreter
+evaluate :: IxIdt -> Int -> Interpreter
 evaluate fIdt x = do
     validInput <- inBounds x
     if validInput
         then do
-            dictionary <- functionDictionary <$> StateT.get
-            f <- case Map.lookup fIdt dictionary of
-                    (Just f) -> return f
-                    Nothing -> lift $ Left $ "Error: undefined function symbol " ++ show fIdt
-            result <- evaluateUnsafe f x
-            validOutput <- inBounds result
-            if validOutput 
-                then return result
-                else return 0
+            cachedResult <- cacheLookup fIdt x
+            case cachedResult of
+                Nothing -> do
+                    dictionary <- functionDictionary <$> StateT.get
+                    f <- case Map.lookup fIdt dictionary of
+                        (Just f) -> return f
+                        Nothing -> lift $ Left $ "Error: undefined function symbol " ++ show fIdt
+                    result <- evaluateUnsafe f x
+                    validOutput <- inBounds result
+                    let result = if validOutput then result else 0
+                    cacheWrite fIdt x result
+                    return result
+                (Just result) -> return result
         else return 0
 
-evaluateUnsafe :: Fun -> Int -> Interpreter
-evaluateUnsafe ConstOne _ = return 1
-evaluateUnsafe Identity x = return x
-evaluateUnsafe (Predefined vals) x = return $ vals !! x
-evaluateUnsafe (OpFun op lhs rhs) x = do
+evaluateUnsafe :: IxFun -> Int -> Interpreter
+evaluateUnsafe IxConstOne _ = return 1
+evaluateUnsafe IxIdentity x = return x
+evaluateUnsafe (IxPredefined vals) x = return $ vals !! x
+evaluateUnsafe (IxOpFun op lhs rhs) x = do
     lhsResult <- evaluate lhs x
     rhsResult <- evaluate rhs x
     return $ interpretOperator op lhsResult rhsResult
-evaluateUnsafe (RecFun outerFun innerFun) x = do
+evaluateUnsafe (IxRecFun outerFun innerFun) x = do
     eP <- equalPredecessor innerFun x
     boundedApplication outerFun eP x
 
-equalPredecessor :: Idt -> Int -> Interpreter
+equalPredecessor :: IxIdt -> Int -> Interpreter
 equalPredecessor f x = do
     r <- evaluate f x
     let eP (-1) = return x
@@ -146,7 +159,7 @@ equalPredecessor f x = do
                 else eP (i-1)
     eP (x-1)
 
-boundedApplication :: Idt -> Int -> Int -> Interpreter
+boundedApplication :: IxIdt -> Int -> Int -> Interpreter
 boundedApplication f x bound = if x < bound 
     then evaluate f x
     else return x
